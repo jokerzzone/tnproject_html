51f87e54671f0407fb3f0097fff7101b2cc0d42d
贾玲玲 <1351182753@qq.com>
2018-04-17 16:35:05 +0800

    jll-1.0.2
    data-all

--- tianning/js/url.js
+++ tianning/js/url.js
@@ -1,4 +1,4 @@
 var commonUrl = "http://58.216.140.181:8181/xbproject";
 
-//var commonUrl = "http://192.168.1.114:8081/xbproject";
+//var commonUrl = "http://192.168.1.114:8080";
 var mapUrl = "http://58.216.140.181:8181";
